330a2c9c03c945b49df90fd74e74c15b31b54dab
Spourgeon2000 <spourgeon@gmail.com>
2021-06-04 14:48:23 +0530

    First page  without CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Layout from './components/layout/Layout';
 
 import HomePage from './pages/HomePage'
+import Top5 from './pages/test';
 
 function App() {
   return (
     <>
       <Layout>
         <HomePage />
+
       </Layout>
     </>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,48 @@
 import SingleUserForm from "../components/formdata/SingleUserForm";
-import Top5 from "./test";
+import React,{useState,useEffect} from 'react';
+
 
 
 function HomePage() {
-  function exectueThis(username) {}
+
+const [Top5,setTop5]=useState([]);
+const [error,setError]=useState(null);
+
+useEffect(()=> {
+  var date = new Date();
+	var y = date.toISOString();
+	var current_time = "";
+	for(var i = 0; i < y.length - 5; i++) current_time += y[i];
+	const url = "https://aqueous-ravine-73981.herokuapp.com/https://clist.by/api/v2/contest/?resource_id=2&start__gt="+current_time+"&order_by=start&username=spourgeon&api_key=b6c222068d30d8e84925061f83aa79e844f1e94e";
+	fetch(url)
+  .then(res=>res.json())
+  .then((result)=>{
+    var contests=[]
+    console.log(result.objects.length);
+    const size = result.objects.length;
+ for(var i = 0; i < Math.min(5, size); i++) contests.push(result.objects[i].event);
+    setTop5(contests);
+  },
+  (error)=>{
+    setError(error);
+  }
+  )
+
+},[])
+
   return (
     <>
       <section>
-        <SingleUserForm onSubmit={exectueThis} />
+        <SingleUserForm />
       </section>
-      <section>
-        <div>
-	
-		</div>
-      </section>
+  
+    <ul>
+    
+	   {Top5.map((Top,id)=>(
+       <li key={id}>{Top}</li>
+     ))}
+		</ul>
+     
     </>
   );
 }
